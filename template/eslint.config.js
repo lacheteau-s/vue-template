ecfc78f26f9ca02de600ddd4180d72f627ce9349
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-02-10 16:25:56 -0500

    Configure files to ignore

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -6,6 +6,10 @@ import vuelint from 'eslint-plugin-vue';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
+	{
+		name: 'app/files-to-ignore',
+		ignores: ['**/dist/**']
+	},
 	eslint.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
 	vuelint.configs['flat/recommended'],
