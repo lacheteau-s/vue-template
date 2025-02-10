7f73679665b049ef71972ba2a66bb90d0f48df26
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-02-10 10:36:17 -0500

    ESLint plugin for Vue

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -2,13 +2,17 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import vuelint from 'eslint-plugin-vue';
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
+	vuelint.configs['flat/recommended'],
 	{
 		languageOptions: {
 			parserOptions: {
+				parser: tseslint.parser,
+				extraFileExtensions: ['.vue'],
 				projectService: true,
 				tsconfigRootDir: import.meta.dirname
 			}
