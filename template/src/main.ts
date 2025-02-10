04953767757e155e205e2f217ed4430628f1fdbf
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-02-09 21:57:24 -0500

    Type-checked configuration

--- template/src/main.ts
+++ template/src/main.ts
@@ -3,4 +3,6 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// https://github.com/typescript-eslint/typescript-eslint/issues/2865
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount('#app');
