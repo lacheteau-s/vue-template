2accf9cda82290e4918cecb943664463d632ea72
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-02-10 23:38:16 -0500

    Base layout

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,1 +1,10 @@
-<template>Application</template>
+<template>
+	<v-app>
+		<v-app-bar title="Application" />
+		<v-main>
+			<v-container class="fill-height">
+				<v-row class="d-flex justify-center align-center">Main content</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
+</template>
