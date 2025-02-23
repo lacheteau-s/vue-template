117d7c0f4287a8b61df42f2056dcad1ceffa72aa
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-02-23 03:21:28 -0500

    Installation

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,8 +1,13 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 
 // https://github.com/typescript-eslint/typescript-eslint/issues/2865
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.mount('#app');
