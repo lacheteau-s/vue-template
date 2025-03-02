b761fc557c7ada9219eece4fa0b4538226a3b4dc
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-02 00:24:38 -0500

    Vuetify + Pinia template v1.0

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,47 +1,26 @@
 <script setup lang="ts">
-	import HelloWorld from './components/HelloWorld.vue';
-	import TheWelcome from './components/TheWelcome.vue';
+	import AppHeader from '@/components/AppHeader.vue';
+	import { useCounterStore } from '@/stores/counter';
+
+	const counter = useCounterStore();
+
+	function onClick() {
+		counter.increment();
+	}
 </script>
 
 <template>
-	<header>
-		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-		</div>
-	</header>
-
-	<main>
-		<TheWelcome />
-	</main>
+	<v-app>
+		<AppHeader />
+		<v-main>
+			<v-container class="fill-height">
+				<v-row>
+					<v-col cols="12" class="d-flex justify-center align-center">Main content</v-col>
+					<v-col cols="12" class="d-flex justify-center align-center">
+						<v-btn @click="onClick">Clicked: {{ counter.count }}</v-btn>
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
-
-<style scoped>
-	header {
-		line-height: 1.5;
-	}
-
-	.logo {
-		display: block;
-		margin: 0 auto 2rem;
-	}
-
-	@media (min-width: 1024px) {
-		header {
-			display: flex;
-			place-items: center;
-			padding-right: calc(var(--section-gap) / 2);
-		}
-
-		.logo {
-			margin: 0 2rem 0 0;
-		}
-
-		header .wrapper {
-			display: flex;
-			place-items: flex-start;
-			flex-wrap: wrap;
-		}
-	}
-</style>
