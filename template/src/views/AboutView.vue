a1acb6c1d1de2b7dfce4c2c4ff085b63673b7bd8
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-02 00:07:53 -0500

    Vuetify + Router template v1.0

---
+++ template/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+	<v-main>
+		<v-container class="fill-height">
+			<v-row class="d-flex justify-center align-center">About content</v-row>
+		</v-container>
+	</v-main>
+</template>
--- template/vite.config.ts
+++ template/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import fonts from 'unplugin-fonts/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +12,28 @@ export default defineConfig(({ mode }) => {
 	const port = Number(env.PORT);
 
 	return {
-		plugins: [vue(), vueDevTools()],
+		plugins: [
+			vue({
+				template: { transformAssetUrls }
+			}),
+			vueDevTools(),
+			vuetify({
+				autoImport: true,
+				styles: {
+					configFile: 'src/styles/settings.scss'
+				}
+			}),
+			fonts({
+				google: {
+					families: [
+						{
+							name: 'Roboto',
+							styles: 'wght@100;300;400;500;700;900'
+						}
+					]
+				}
+			})
+		],
 		resolve: {
 			alias: {
 				'@': fileURLToPath(new URL('./src', import.meta.url))
